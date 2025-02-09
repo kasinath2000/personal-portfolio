69f805abf63fd01b6e0dc7c1eb8c531d194c3d21
kasinath2000 <kasi21.12.2000@gmail.com>
2025-02-09 19:36:46 +0530

    add image inside color

--- src/App.jsx
+++ src/App.jsx
@@ -23,3 +23,35 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// import { CssBaseline, Container, Box } from "@mui/material";
+// import Navbar from "./components/NavBar";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Skills from "./components/Skills/Skills";
+// import Projects from "./components/Projects/Projects";
+// import Contact from "./components/Contact/Contact";
+// import Footer from "./components/Footer/Footer";
+// import "./App.css";
+
+// function App() {
+//   return (
+//     <Box sx={{ backgroundColor: "primary.main", color: "white", minHeight: "100vh" }}>
+//       <CssBaseline />
+//       <Navbar />
+//       <Container maxWidth="lg" component="main">
+//         <Home />
+//         <About />
+//         <Skills />
+//         <Projects />
+//         <Contact />
+//       </Container>
+//       <Footer />
+//     </Box>
+//   );
+// }
+
+// export default App;
